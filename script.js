c0dd94de996ce72a8679568e9702dff06e90ca1d
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 13:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let p= document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+let counter = 0;
+p.innerText = counter;
+btn.addEventListener("click", onClick);
+
+function onClick() {
+	alert(counter);
+	counter++;
+	p.innerText = counter;
+}
